123d415297c8d746296181df2f80ecc4a6964b5e
Miha Markič <miha@rthand.com>
2022-04-05 15:12:34 +0200

    Adds initial files

---
+++ src/IL2Amiga/IL2Amiga.Engine/Compiler.cs
@@ -0,0 +1,10 @@
+﻿namespace IL2Amiga.Engine
+{
+    public class Compiler
+    {
+        public void Compile(string entryAssemblyFileName, IEnumerable<string> assembliesFileNames, TextWriter writter)
+        {
+            writter.WriteLine("Test");
+        }
+    }
+}
---
+++ src/IL2Amiga/IL2Amiga/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Immutable;
+using System.Text;
+using IL2Amiga.Engine;
+
+string entryAssemblyFileName = args[0];
+
+var compiler = new Compiler();
+var sb = new StringBuilder();
+var writer = new StringWriter(sb);
+compiler.Compile(entryAssemblyFileName, ImmutableArray<string>.Empty, writer);
+Console.WriteLine(writer.ToString());
